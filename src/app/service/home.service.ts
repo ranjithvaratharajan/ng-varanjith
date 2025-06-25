074096db51951819601454182b7b384c33278538
Ranjith Varatharajan <ranjith.varatharajan@pm.me>
2025-06-25 19:38:54 +0530

    Temporarily removed portfolio section and code cleanup

--- src/app/service/home.service.ts
+++ src/app/service/home.service.ts
@@ -33,7 +33,7 @@ export class HomeService {
     { path: 'about-me', label: 'About Me', icon: 'pw-icon-user' },
     { path: 'resume', label: 'Resume', icon: 'pw-icon-vcard' },
     { path: 'contact', label: 'Contact', icon: 'pw-icon-at' },
-    { path: 'portfolio', label: 'Portfolio', icon: 'pw-icon-lightbulb' },
+    //{ path: 'portfolio', label: 'Portfolio', icon: 'pw-icon-lightbulb' },
     { path: 'admin', label: 'Admin', icon: 'pw-icon-lock' },
   ]);
   error = signal<string | null>(null);
